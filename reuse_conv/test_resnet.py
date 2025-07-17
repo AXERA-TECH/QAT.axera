435bbd50385a2a636631142ba5f79219e918e2ad
Jackpot233333 <39452287+Jackpot233333@users.noreply.github.com>
2025-07-17 11:59:20 +0800

    add reuse conv demo (#24)
    Co-authored-by: liyuchen <liyuchen@axera-tech.com>

---
+++ reuse_conv/test_resnet.py
@@ -0,0 +1,64 @@
+import torch
+import onnxruntime as ort
+
+from torch.ao.quantization.quantizer.xnnpack_quantizer import (
+    XNNPACKQuantizer,
+    get_symmetric_quantization_config,
+)
+from torch.ao.quantization.quantize_pt2e import (
+    prepare_qat_pt2e,
+    convert_pt2e,
+)
+
+# from utils.quantizer import (
+#     AXQuantizer,
+#     get_quantization_config,
+# )
+from utils.ax_quantizer import(
+    load_config,
+    AXQuantizer,
+    remove_reused_bn_param_hack,
+)
+from utils.train_utils import (
+  load_model,
+  evaluate,
+  evaluate_np,
+  imagenet_data_loaders,
+)
+from reuse_conv.train_resnet import ReuseResNet, Bottleneck
+
+def test():
+    # float model
+    # float_model = load_model("./resnet50/resnet50_pretrained_float.pth", "resnet50").to("cuda")
+    float_model = ReuseResNet(Bottleneck, [3, 4, 6, 3]).to("cuda")
+    state_dict = torch.load("./resnet50/resnet50_pretrained_float.pth", weights_only=True)
+    float_model.load_state_dict(state_dict, strict=False)
+
+    # quantizer
+    global_config, regional_configs = load_config("./reuse_conv/config.json")
+    quantizer = AXQuantizer(annotate_bias=False)
+    quantizer.set_global(global_config)
+    quantizer.set_regional(regional_configs)
+
+    # quant model
+    example_inputs = (torch.rand(1, 3, 224, 224).to("cuda"),)
+    exported_model = torch.export.export_for_training(float_model, example_inputs).module()
+    prepared_model = prepare_qat_pt2e(exported_model, quantizer)
+
+    prepared_model.load_state_dict(torch.load("./reuse_conv/resnet50.pth"))
+    remove_reused_bn_param_hack(prepared_model)
+    quantized_model = convert_pt2e(prepared_model)
+
+    # onnx session
+    sess = ort.InferenceSession("./reuse_conv/resnet50_qat_sim.onnx")
+
+    # dataset
+    data_loader, data_loader_test = imagenet_data_loaders("dataset/imagenet/")
+
+    # evaluate
+    top1, top5 = evaluate(quantized_model, data_loader_test, total_size=100)
+    top1, top5 = evaluate_np(sess, data_loader_test, total_size=100)
+
+
+if __name__ == "__main__":
+    test()
